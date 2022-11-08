e7fca5b63188b5d41b733d3f670b6354f3acced7
Steven-Burlett <stevenburlett@gmail.com>
2022-11-07 22:19:50 -0500

    add user model and registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   get "about", to:  "about#index"
   root to: "main#index"
 end
